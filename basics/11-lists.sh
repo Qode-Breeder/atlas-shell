1e0dfcc6e42e69bc01182e4e0d51a11cbeb30430
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-21 14:02:01 -0500

    List in long format with hidden file for current dir and its parent and /boot

---
+++ basics/11-lists.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# Write a script that lists all file including hidden ones,
+# in the current dir and the parent of the working dir and /boot directory,
+# in long format.
+
+ls -la . .. /boot

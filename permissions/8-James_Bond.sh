5cfd7844b2e0cec8c1b893bcfa77f1a6a3484736
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-27 02:41:25 -0500

    Add no permissions to user and user group, but all permissions to others

---
+++ permissions/8-James_Bond.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Write a script that sets the permissions to the file hello as follows:
+# Owner: no permissions
+# Group: no permissions
+# Other users: all permissions
+
+sudo chmod 007 # 0=0, 7= r:4, w:2, x:1 (7)

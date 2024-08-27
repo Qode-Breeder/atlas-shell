a6dfd57bfa51abee82af77824f35c5c44d9845ae
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-27 02:38:02 -0500

    Add execution permissions to everyone

---
+++ permissions/7-everybody.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# Write a script that adds execution permissions to the
+# the owner, group owner and others, to the file hello
+
+sudo chmod +x hello # This will add execution permissions to everyone

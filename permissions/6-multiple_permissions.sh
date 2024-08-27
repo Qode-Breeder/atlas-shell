b962ee8ea44562726049490499e08396e57f6ca5
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-27 02:31:43 -0500

    Add multiple permissions to a given file

--- permissions/6-multiple_permissions.sh
+++ permissions/6-multiple_permissions.sh
@@ -3,4 +3,4 @@
 # Write a script that add execute permissions to owner and owner group
 # and read permissions to other users to the file hello.
 
-sudo chmod 114 # 4=read, 2=write, 1=execute
+sudo chmod 114 hello # 4=read, 2=write, 1=execute

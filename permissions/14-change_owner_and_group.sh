a26d9a8a3b845e2b816df9d08236bcf2062cac02
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-28 20:27:00 -0500

    change the owner and group owner of all files and dirs in the currnet working dir recursively

---
+++ permissions/14-change_owner_and_group.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# Write a script that changes the owner to vincent and the 
+# group owner to staff for all files and diretories in the working dir.
+
+sudo chown -R vincent:staff

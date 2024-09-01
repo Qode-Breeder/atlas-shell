2b61c3de90f984c10c24755054880999ad8df870
Frandy Slueue <Frandy4ever@gmail.com>
2024-09-01 17:07:29 -0500

    Print Hello, World to standard output

---
+++ io_redirections_and_filters/0-hello_world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Write a script that prints "Hello, World",
+# followed by a new line to the standard output.
+
+echo -e "Hello, World\n" # -e enables interpretation of backslash escape.

d642753f18eb201d035cf7e328b7db53b0ce6b82
Frandy Slueue <Frandy4ever@gmail.com>
2024-09-02 06:06:46 -0500

    Print exactly the follow string "\*\\'"Best School"\'\\*$\?\*\*\*\*\*:)"

---
+++ io_redirections_and_filters/7-file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Write a shell script tht creates a file named exactly
+# \*\\'"Best School"\'\\*$\?\*\*\*\*\*:) containing the text
+# Best School ending by a new line.
+
+echo -E "\*\\\'\"Best School\"\'\\\*$\?\*\*\*\*\*:)"
